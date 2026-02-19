61d65571e6202bf88e23d117e55ef64673b4dc08
Jeffred Lim <58154782+jepjepj@users.noreply.github.com>
2026-02-19 13:33:29 +0800

    env

--- core/env.php
+++ core/env.php
@@ -1,11 +1,11 @@
 <?php
 
 define('APP_FOLDER', 'rehabilation-manager-be');
-define('BASE_URL', 'https://localhost' . APP_FOLDER . '/');
+define('BASE_URL', 'https://rehabmanager.org' . APP_FOLDER . '/');
 define("BASE_PATH", __DIR__ . "/rehabilation-manager-be/");
 define('DEVELOPMENT', false);
 
 define("HOST", "72.60.232.151");
-define("USER", "rehab_manager_root");
+define("USER", "rehab_center_root");
 define("PASSWORD", "Z#2+771n6LSmJkdR");
 define("DBNAME", "rehab_management_main_db");
